171299b96318e580d61aa3549b6c2e58c959e288
Renato Matos <renato.matos@creditas.comb.br>
2020-08-03 14:48:10 -0300

    Adding supplier POST and FindById

--- src/main/kotlin/com/sgo/sgo/controllers/SupplierController.kt
+++ src/main/kotlin/com/sgo/sgo/controllers/SupplierController.kt
@@ -1,6 +1,8 @@
 package com.sgo.sgo.controllers
 
+import com.sgo.sgo.entities.ClientInputDTO
 import com.sgo.sgo.entities.ClientOutputDTO
+import com.sgo.sgo.entities.SupplierInputDTO
 import com.sgo.sgo.entities.SupplierOutputDTO
 import com.sgo.sgo.services.ClientService
 import com.sgo.sgo.services.PersonAddressService
@@ -10,10 +12,10 @@ import com.sgo.sgo.utils.decodeParam
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import javax.transaction.Transactional
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/suppliers")
@@ -22,6 +24,12 @@ class SupplierController {
     @Autowired
     lateinit var supplierService: SupplierService
 
+    @Autowired
+    lateinit var personService: PersonService
+
+    @Autowired
+    lateinit var personAddressService: PersonAddressService
+
     @GetMapping
     @Operation(summary = "List suppliers")
     fun listAll(@RequestParam(value = "name", required = false) name: String?,
@@ -29,5 +37,27 @@ class SupplierController {
         val nameToBeFound: String? = if (name != null) decodeParam(name) else null
         var suppliers = supplierService.listSuppliers(document, nameToBeFound)
         return ResponseEntity.ok().body(suppliers)
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary="Find a supplier by id")
+    fun findById(@PathVariable id: Long) : ResponseEntity<SupplierOutputDTO> {
+        val supplier = supplierService.findById(id)
+        return ResponseEntity.ok().body(supplier)
+    }
+
+    @PostMapping
+    @Operation(summary = "Insert a new supplier")
+    @Transactional
+    fun insert(@Valid @RequestBody supplierDTO : SupplierInputDTO) : ResponseEntity<Void> {
+        val supplier = supplierService.fromInputDTO(supplierDTO)
+        val personInserted = personService.insert(supplier.person)
+        supplier.person.personAddresses.forEach {
+            it.person=personInserted
+            personAddressService.insert(it)
+        }
+        val supplierInserted = supplierService.insert(supplier)
+        val uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(supplierInserted.id).toUri()
+        return ResponseEntity.created(uri).build()
     }
 }
