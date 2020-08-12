89f9890f9a6cc60b71f174c5e5bb7534de0c8179
Renato Matos <renato.matos@creditas.com.br>
2020-08-12 16:42:34 -0300

    Changing Keycloak client id and secret by environment variables

--- src/main/kotlin/com/sgo/sgo/services/CustomAuthenticationService.kt
+++ src/main/kotlin/com/sgo/sgo/services/CustomAuthenticationService.kt
@@ -18,11 +18,13 @@ class CustomAuthenticationService {
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
 
+        val clientId = System.getenv("keycloak-client-id")
+        val clientSecret = System.getenv("keycloak-client-secret")
         val arguments = LinkedMultiValueMap<String, String>()
-        arguments.add("grant_type","password")
-        arguments.add("client_id","sgo-kotlin")
-        arguments.add("client_secret","43d18a4c-ae80-420d-8355-2429f1829a23")
-        arguments.add("username",username)
+        arguments.add("grant_type", "password")
+        arguments.add("client_id", clientId)
+        arguments.add("client_secret", clientSecret)
+        arguments.add("username", username)
         arguments.add("password", password)
 
         val req = HttpEntity<MultiValueMap<String, String>>(arguments, headers)
